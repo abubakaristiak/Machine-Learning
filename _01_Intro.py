3cf725b3f6744a0e6a292018434e0d1b6d931637
Abu Bakar Istiak <151659711+abubakaristiak@users.noreply.github.com>
2024-03-12 09:19:45 +0600

    Add files via upload

---
+++ _01_Intro.py
@@ -0,0 +1,5 @@
+# Introduction to Machine Learning:
+
+'''
+Machine learning is a subfield of artificial intelligence that uses algorithms trained on data sets to create models. These models enable machines to perform tasks that would otherwise only be possible for humans.
+''' 
