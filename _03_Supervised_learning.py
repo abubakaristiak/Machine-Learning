1042a640b5bcbba9c80bf31ab6eddfcb8b5802b2
Abu Bakar Istiak <151659711+abubakaristiak@users.noreply.github.com>
2024-03-12 09:43:07 +0600

    Theory

---
+++ _03_Supervised_learning.py
@@ -0,0 +1,5 @@
+# Supervised machine learning
+
+'''
+Supervised machine learning is a type of machine learning that learns the relationship between input and output. The inputs are known as features or "X variables" and output is generally referred to as the target or "y variable".
+'''
