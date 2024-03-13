2f40f5306528ef53722007c8f1e157f85dc49823
Abu Bakar Istiak <151659711+abubakaristiak@users.noreply.github.com>
2024-03-13 12:54:28 +0600

    Theory

---
+++ _04_Type_of_models.py
@@ -0,0 +1,39 @@
+'''
+
+Machine learning models can be broadly categorized into three main types based on the learning style and approach:
+
+Supervised Learning:
+In supervised learning, the model learns from labeled data, which means that the training data includes input-output pairs.
+The goal is to learn a mapping from inputs to outputs, based on the patterns present in the labeled data.
+Common algorithms include:
+Linear Regression
+Logistic Regression
+Decision Trees
+Random Forests
+Support Vector Machines (SVM)
+Neural Networks (in the context of classification/regression tasks)
+
+
+
+Unsupervised Learning:
+Unsupervised learning deals with unlabeled data, where the algorithm tries to find patterns or structures in the data without explicit guidance.
+The model tries to learn the underlying structure of the data, such as clustering similar data points together or reducing the dimensionality of the data.
+Common algorithms include:
+K-Means Clustering
+Hierarchical Clustering
+Principal Component Analysis (PCA)
+t-Distributed Stochastic Neighbor Embedding (t-SNE)
+Autoencoders
+Generative Adversarial Networks (GANs)
+
+
+Reinforcement Learning:
+Reinforcement learning involves an agent learning to make decisions by interacting with an environment.
+The agent learns from the consequences of its actions, receiving rewards or penalties for the actions it takes.
+The goal is to learn a policy that maximizes the cumulative reward over time.
+Common algorithms include:
+Q-Learning
+Deep Q-Networks (DQN)
+Policy Gradient Methods
+Actor-Critic Methods
+'''
