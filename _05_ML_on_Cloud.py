b7e943337e1352af2a1b9cb6e2dc0c05a85b05ba
Abu Bakar Istiak <151659711+abubakaristiak@users.noreply.github.com>
2024-03-14 23:47:28 +0600

    Theory

---
+++ _05_ML_on_Cloud.py
@@ -0,0 +1,22 @@
+'''
+What is the role of Cloud in Machine Learning
+
+Cloud computing and ML are two different technologies with distinct purposes, but they can be used together to achieve powerful results. Cloud computing provides storage and security for ML applications, which require a lot of computational power. Cloud computing also offers a pay-per-use model that allows companies to leverage ML capabilities without much expenditure.
+
+ 
+Some popular cloud computing platforms for ML include:
+1. Amazon Web Services (AWS)
+2. Microsoft Azure
+3. Google Cloud
+4. IBM Cloud 
+
+
+
+Here are some ML platforms in the cloud:
+1. Google Cloud Machine Learning: Lets users train models on their data in an automated way, with no or limited data science expertise
+2. Google Cloud AI Platform: Best for large-scale machine learning tasks
+3. AWS SageMaker: Best for integrating with AWS services
+4. Databricks Unified Data Analytics: Best for Apache Spark-based analytics
+ 
+All of these cloud service providers offer trial ML solutions so that clients can evaluate the capabilities of the chosen platform before moving to a paid service. 
+'''
